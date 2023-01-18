d377ece1a837d703077510cac3cffb8a74aa053d
rafifaditio <89371310+rafifaditio@users.noreply.github.com>
2023-01-18 15:57:05 +0700

    Add files via upload

--- deploy_huggingface/app.py
+++ deploy_huggingface/app.py
@@ -12,10 +12,10 @@ st.write('Insert feature to predict')
 # user input
 ph = st.slider(label='pH', min_value=3.0, max_value=9.5, value=6.5, step=0.1)
 temp = st.slider(label='Temprature', min_value=34, max_value=90, value=40, step=1)
-taste = st.selectbox(label='Taste', options=['Good', 'Bad'], key=0)
-odor = st.selectbox(label='Odor', options=['Good', 'Bad'], key=0)
-fat = st.selectbox(label='Fat', options=['High', 'Low'], key=1)
-turb = st.selectbox(label='Turbidity', options=['High', 'Low'], key=1)
+taste = st.selectbox(label='Taste', options=['Good', 'Bad'])
+odor = st.selectbox(label='Odor', options=['Good', 'Bad'])
+fat = st.selectbox(label='Fat', options=['High', 'Low'])
+turb = st.selectbox(label='Turbidity', options=['High', 'Low'])
 color = st.number_input(label='Colour', min_value=240, max_value=255, value=245, step=1)
 
 # convert into dataframe
